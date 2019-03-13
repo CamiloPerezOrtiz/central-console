1b3de89373bbca611c77d6b8d35f43e5a700f018
camilo <camilo.perez.ort@gmail.com>
2019-03-13 10:52:59 -0600

    EXPOSICION 13 de marzo

--- src/PrincipalBundle/Controller/FirewallController.php
+++ src/PrincipalBundle/Controller/FirewallController.php
@@ -143,51 +143,85 @@ class FirewallController extends Controller
 	public function listaFirewallWanAction()
 	{
 		$u = $this->getUser();
-		if($u != null)
+		$role=$u->getRole();
+		if($role == 'ROLE_SUPERUSER')
 		{
-	        $role=$u->getRole();
-	        $grupo=$u->getGrupo();
-	        if($role == "ROLE_SUPERUSER")
-	        {
-	        	$id=$_REQUEST['id'];
-				$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($id);
-				#$natOne = $this->recuperarOneToOneGrupo($id);
-				return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
-					"firewallWan"=>$firewallWan,
-					#"natOne"=>$natOne
-				));
-	        }
-	        if($role == "ROLE_ADMINISTRATOR")
-	        {
-	        	$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($grupo);
-	        	#$natOne = $this->recuperarOneToOneGrupo($grupo);
-				return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
-					"firewallWan"=>$firewallWan,
-					#"natOne"=>$natOne
-				));
-	        }
-	        if($role == "ROLE_USER")
-	        {
-	        	$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($grupo);
-	        	#$natOne = $this->recuperarOneToOneGrupo($grupo);
-				return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
-					"firewallWan"=>$firewallWan,
-					#"natOne"=>$natOne
-				));
-	        }
-	    }
+			$grupo=$_REQUEST['id'];
+			$ipGrupos = $this->ipGrupos($grupo);
+		}
+		else
+		{
+			$grupo=$u->getGrupo();
+			$ipGrupos = $this->ipGrupos($grupo);
+		}
+		if(isset($_POST['solicitar']))
+		{
+			#$u = $this->getUser();
+			if($u != null)
+			{
+		        #$role=$u->getRole();
+		        #$grupo=$u->getGrupo();
+		        $ubicacion = $_REQUEST['ubicacion'];
+		        if($role == "ROLE_SUPERUSER")
+		        {
+		        	$id=$_REQUEST['id'];
+					$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($grupo, $ubicacion);
+					#$natOne = $this->recuperarOneToOneGrupo($id);
+					return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
+						"firewallWan"=>$firewallWan,
+						#"natOne"=>$natOne
+					));
+		        }
+		        if($role == "ROLE_ADMINISTRATOR")
+		        {
+		        	$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($grupo, $ubicacion);
+		        	#$natOne = $this->recuperarOneToOneGrupo($grupo);
+					return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
+						"firewallWan"=>$firewallWan,
+						#"natOne"=>$natOne
+					));
+		        }
+		        if($role == "ROLE_USER")
+		        {
+		        	$firewallWan = $this->recuperarTodoFirewallWanPortGrupo($grupo, $ubicacion);
+		        	#$natOne = $this->recuperarOneToOneGrupo($grupo);
+					return $this->render('@Principal/firewall/listaFirewall.html.twig', array(
+						"firewallWan"=>$firewallWan,
+						#"natOne"=>$natOne
+					));
+		        }
+		    }
+		}
+		return $this->render('@Principal/plantillas/solicitudGrupo.html.twig', array(
+			'ipGrupos'=>$ipGrupos
+		));
 	}
 
 	# Funcion para recuperar los todos los aliases #
-	private function recuperarTodoFirewallWanPortGrupo($grupo)
+	private function recuperarTodoFirewallWanPortGrupo($grupo, $ubicacion)
 	{
 		$em = $this->getDoctrine()->getEntityManager();
 	    $db = $em->getConnection();
-		$query = "SELECT * FROM firewall_lan WHERE grupo ='$grupo' ORDER BY posicion";
+		$query = "SELECT * FROM firewall_lan WHERE grupo ='$grupo' AND ubicacion = '$ubicacion' ORDER BY posicion";
 		$stmt = $db->prepare($query);
 		$params =array();
 		$stmt->execute($params);
 		$nat=$stmt->fetchAll();
 		return $nat;
+	}
+
+	public function dragAndDropFirewallWanAction()
+	{
+		$em = $this->getDoctrine()->getEntityManager();
+		$db = $em->getConnection();
+		$position = $_POST['position'];
+		$i=1;
+		foreach($position as $k=>$v){
+		    $sql = "Update firewall_lan SET posicion=".$i." WHERE id=".$v;
+		    $stmt = $db->prepare($sql);
+			$stmt->execute(array());
+			$i++;
+		}
+		exit();
 	}
 }
